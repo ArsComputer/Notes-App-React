cbf7e5b858f5786e3f6f0748af68fa383ccf8b89
okComputer <mohrasyaar2008@gmail.com>
2026-09-08 22:03:40 +0700

    disable the use state temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [note, setNote] = useState('')
 
   return (
     <>
+      
     </>
   )
 }
